577c06e61a383c0a2905c980e75ec1aea50048a5
Sergey Malakanov <sergey.malakanov@gmail.com>
2019-10-11 09:40:44 +0500

    filter add

--- src/components/Todoitem.vue
+++ src/components/Todoitem.vue
@@ -1,62 +1,61 @@
 <template>
-	<li>
-		<span v-bind:class="{ done: todo.completed }">
-			<input
-				type="checkbox"
-				v-on:change="todo.completed = !todo.completed"
-			/>
+  <li>
+    <span v-bind:class="{ done: todo.completed }">
+      <input type="checkbox" v-on:change="todo.completed = !todo.completed" />
 
-			<strong>{{ index + 1 }}</strong>
+      <strong>{{ index + 1 }}</strong>
+      {{ todo.title | uppercase }}
+    </span>
 
-			{{ todo.title }}
-		</span>
-
-		<button class="rm" v-on:click="$emit('remove-todo', todo.id)">
-			&times;
-		</button>
-	</li>
+    <button class="rm" v-on:click="$emit('remove-todo', todo.id)">&times;</button>
+  </li>
 </template>
 
 <script>
 export default {
-	props: {
-		todo: {
-			type: Object,
-			required: true
-		},
-		index: Number
+  props: {
+    todo: {
+      type: Object,
+      required: true
+    },
+    index: Number
+	},
+	filters: {
+		uppercase (value) {
+			return value.toUpperCase()
+		}
 	}
 };
 </script>
 
 <style>
 li {
-	border: 1px solid #ccc;
-	display: flex;
-	justify-content: space-between;
-	padding: 0.5rem 0.2rem;
-	margin-bottom: 1rem;
+  border: 1px solid #ccc;
+  display: flex;
+  justify-content: space-between;
+  padding: 0.5rem 0.2rem;
+  margin-bottom: 1rem;
 }
 
 .rm {
-	background: red;
-	color: #fff;
-	border-radius: 50%;
-	font-weight: bold;
+  background: red;
+  color: #fff;
+  border-radius: 50%;
+  font-weight: bold;
 }
 
 .add {
-	background: green;
-	color: #fff;
-	border-radius: 50%;
-	font-weight: bold;
+  background: green;
+  color: #fff;
+  border-radius: 50%;
+  font-weight: bold;
 }
 
 .done {
-	text-decoration: line-through;
+  text-decoration: line-through;
 }
 
 input {
-	margin-right: 1rem;
+  margin-right: 1rem;
 }
 </style>
--- src/views/Todos.vue
+++ src/views/Todos.vue
@@ -3,8 +3,23 @@
     <h2>Todo application</h2>
 
     <AddTodo @add-todo="addTodo" />
+    <select v-model="filter">
+<option value="all">All</option>
+<option value="completed">Completed</option>
+<option value="not-completed">Not-completed</option>
 
-    <Todolist v-bind:todos="todos" v-on:remove-todo="removeTodo" v-on:add-todo="addTodo" />
+    </select>
+
+    <Loader v-if="loading" />
+
+    <Todolist
+      v-else-if="todos.length"
+      v-bind:todos="filteredTodos"
+      v-on:remove-todo="removeTodo"
+      v-on:add-todo="addTodo"
+    />
+    
+    <a href="/">Home</a>
   </div>
 </template>
 
@@ -13,26 +28,48 @@
 <script>
 import Todolist from "@/components/Todolist.vue";
 import AddTodo from "@/components/AddTodo.vue";
+import Loader from "@/components/Loader.vue";
 
 export default {
   name: "app",
 
   data() {
     return {
-      todos: []
+      todos: [],
+      loading: true,
+      filter: 'all'
     };
   },
   mounted() {
     fetch("https://jsonplaceholder.typicode.com/todos?_limit=5")
       .then(response => response.json())
       .then(json => {
-        this.todos = json;
+        setTimeout(() => {
+          this.todos = json;
+          this.loading = false;
+        }),
+          1000;
       });
   },
 
+computed: {
+  filteredTodos() {
+    if (this.filter === 'all'){
+      return this.todos
+    }
+    if (this.filter === 'completed'){
+      return this.todos.filter(t => t.completed )
+    }
+    if (this.filter === 'not-completed'){
+      return this.todos.filter(t => !t.completed )
+    }
+  }
+},
+
   components: {
     Todolist,
-    AddTodo
+    AddTodo,
+    Loader
   },
   methods: {
     removeTodo(id) {
